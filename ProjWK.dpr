6aabc7c38254a9089ac13693f511957d85a18353
fortesm <fortes.m@gmail.com>
2021-12-04 01:57:32 -0300

    Alterações para novo teste

--- ProjWK.dpr
+++ ProjWK.dpr
@@ -3,11 +3,13 @@
 uses
   Vcl.Forms,
   View.Principal in 'View.Principal.pas' {FrmPrincipal},
-  View.PedidoVendas in 'View.PedidoVendas.pas' {FrmPedidoVendas},
-  Consultas in 'Model\Consultas.pas',
   UDMPrincipal in 'Model\UDMPrincipal.pas' {DM: TDataModule},
   View.SelCliente in 'View.SelCliente.pas' {FrmSelCliente},
-  View.ManutPedidos in 'View.ManutPedidos.pas' {FrmManutPedidos};
+  View.ManutPedidos in 'View.ManutPedidos.pas' {FrmManutPedidos},
+  ClassProdutos in 'Model\ClassProdutos.pas',
+  ClassClientes in 'Model\ClassClientes.pas',
+  View.PedidoVendas in 'View.PedidoVendas.pas' {FrmPedidoVendas},
+  ClassPedidos in 'Model\ClassPedidos.pas';
 
 {$R *.res}
 
@@ -15,9 +17,9 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFrmPrincipal, FrmPrincipal);
-  Application.CreateForm(TFrmPedidoVendas, FrmPedidoVendas);
   Application.CreateForm(TDM, DM);
   Application.CreateForm(TFrmSelCliente, FrmSelCliente);
   Application.CreateForm(TFrmManutPedidos, FrmManutPedidos);
+  Application.CreateForm(TFrmPedidoVendas, FrmPedidoVendas);
   Application.Run;
 end.
